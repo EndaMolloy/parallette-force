cc6a37018f4c9c7d98b6663f403e15085528db0b
Enda <enda.molloy.1@gmail.com>
2021-02-20 13:41:20 +0000

    wifi connect with wifi manager

--- sketch_fsr/sketch_fsr.ino
+++ sketch_fsr/sketch_fsr.ino
@@ -1,18 +1,31 @@
+#include <ESP8266WiFi.h>          //ESP8266 Core WiFi Library (you most likely already have this in your sketch)
+#include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
+#include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
+
 
 int forcePin = A0;
 int forceReading;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600); 
+  Serial.begin(9600);
+
+  WiFiManager wifiManager;
+  //reset saved settings
+ 
+  wifiManager.autoConnect("AutoConnectAP");
+
+  //if you get here you have connected to the WiFi
+  Serial.println("connected...yeey :)");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   
   forceReading = analogRead(forcePin);
-  //Serial.print("Analog reading = ");
-  //Serial.println(forceReading);
+  Serial.print("Analog reading = ");
+  Serial.println(forceReading);
  
   delay(500);
 
